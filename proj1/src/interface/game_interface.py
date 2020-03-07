c358315417f337dcf2e2e81a4bd9b4592bac4313
Miguel Pinto <pt.miguel99@gmail.com>
2020-03-07 17:43:00 +0000

    Added image import and display

--- proj1/src/interface/game_interface.py
+++ proj1/src/interface/game_interface.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 from .colors import Colors
 
 # screen dimensions
@@ -23,6 +24,10 @@ class GameInterface:
         self.screen = pygame.display.set_mode([self.screen_width, self.screen_height])
         pygame.display.set_caption("Neutron")
 
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        self.black_soldier = pygame.image.load(current_dir + "/../../res/pawn_black.png")
+        self.white_soldier = pygame.image.load(current_dir + "/../../res/pawn_white.png")
+        self.neutron = pygame.image.load(current_dir + "/../../res/neutron.png")
 
 
 
@@ -44,7 +49,6 @@ class GameInterface:
 
         # Add a nice boarder
         pygame.draw.rect(self.screen, Colors.BOARD_BORDER_COLOR.value, [40, 50, self.board_size * self.square_size, self.board_size * self.square_size], 1)
-        pygame.display.flip()
 
 
 
@@ -67,6 +71,10 @@ class GameInterface:
         Method to draw a node and its board on the pygame interface.
         """
         self.draw_background()
+        self.screen.blit(self.neutron, (0, 0))
+        #self.screen.blit(self.black_soldier, (1, 1))
+        #self.screen.blit(self.black_soldier, (-250, 1))
+        pygame.display.flip()
 
 
 
