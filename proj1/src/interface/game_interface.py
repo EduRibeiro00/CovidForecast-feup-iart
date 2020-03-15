c7a9110b41a786adfdf04a5b126c70f29b6975c5
lockdownpt <guerra70@live.com.pt>
2020-03-15 13:43:36 +0000

    fixed the termination function

--- proj1/src/interface/game_interface.py
+++ proj1/src/interface/game_interface.py
@@ -166,26 +166,52 @@ class GameInterface:
         """
         font = pygame.font.Font(FONT_FILE, FONT_SIZE)
 
+        if state != PlayState.PLAYER_B_WINS and state != PlayState.PLAYER_A_WINS:
+            # create a text surface object, on which text is drawn on it.
+            current_player_text = font.render('Current player', True, Colors.TEXT_COLOR.value, Colors.TEXT_BACKGROUND_COLOR.value)
+            move_piece_text = font.render('Move piece', True, Colors.TEXT_COLOR.value, Colors.TEXT_BACKGROUND_COLOR.value)
+
+            # create a rectangular object for the text surface object
+            current_player_text_rect = current_player_text.get_rect()
+            move_piece_text_rect = current_player_text.get_rect()
+
+            # set the center of the rectangular object.
+            current_player_text_rect.center = (self.screen_width - 100 , self.screen_height // 4)
+            move_piece_text_rect.center = (self.screen_width - 85, self.screen_height // 4 + self.soldier_height + 50)
+
+            self.screen.blit(current_player_text, current_player_text_rect)
+            self.screen.blit(move_piece_text, move_piece_text_rect)
+
+            current_player_image, move_piece_image = self.calc_imgs_turn_info(state)
+
+            self.screen.blit(current_player_image, (self.screen_width - 135, self.screen_height // 4 + 15))
+            self.screen.blit(move_piece_image, (self.screen_width - 135, self.screen_height // 4 + self.soldier_height * 2 ))
+
+        else:
         # create a text surface object, on which text is drawn on it.
-        current_player_text = font.render('Current player', True, Colors.TEXT_COLOR.value, Colors.TEXT_BACKGROUND_COLOR.value)
-        move_piece_text = font.render('Move piece', True, Colors.TEXT_COLOR.value, Colors.TEXT_BACKGROUND_COLOR.value)
+            winner_player_text , winner_player_image = self.calc_img_text_winner(state, font)
+            winner_player_text_rect = winner_player_text.get_rect()
 
-        # create a rectangular object for the text surface object
-        current_player_text_rect = current_player_text.get_rect()
-        move_piece_text_rect = current_player_text.get_rect()
+            winner_player_text_rect.center = (self.screen_width - 100, self.screen_height // 4)
 
-        # set the center of the rectangular object.
-        current_player_text_rect.center = (self.screen_width - 100 , self.screen_height // 4)
-        move_piece_text_rect.center = (self.screen_width - 85, self.screen_height // 4 + self.soldier_height + 50)
+            self.screen.blit(winner_player_text, winner_player_text_rect)
+            self.screen.blit(winner_player_image, (self.screen_width - 135, self.screen_height // 4 + 15))
 
-        self.screen.blit(current_player_text, current_player_text_rect)
-        self.screen.blit(move_piece_text, move_piece_text_rect)
+    def calc_img_text_winner(self, state, font):
 
-        current_player_image, move_piece_image = self.calc_imgs_turn_info(state)
+        if state == PlayState.PLAYER_A_WINS:
+            winner_player_text = font.render('WHITE PLAYER WINS', True, Colors.TEXT_COLOR.value,
+                                             Colors.TEXT_BACKGROUND_COLOR.value)
+            winner_player_image = self.white_soldier_img
+        elif state == PlayState.PLAYER_B_WINS:
+            winner_player_text = font.render('BLACK PLAYER WINS', True, Colors.TEXT_COLOR.value,
+                                             Colors.TEXT_BACKGROUND_COLOR.value)
+            winner_player_image = self.black_soldier_img
+        else:
+            winner_player_text = None
+            winner_player_image = None
 
-        print(current_player_image, move_piece_image)
-        self.screen.blit(current_player_image, (self.screen_width - 135, self.screen_height // 4 + 15))
-        self.screen.blit(move_piece_image, (self.screen_width - 135, self.screen_height // 4 + self.soldier_height * 2 ))
+        return  winner_player_text, winner_player_image
 
     def calc_imgs_turn_info(self, state):
         """
@@ -227,5 +253,6 @@ class GameInterface:
         Method to exit.
         """
         pygame.quit()
+
 
 
