b386558d59f7596b5c786a7cbaf7baea15253bb0
lockdownpt <guerra70@live.com.pt>
2020-03-21 12:06:30 +0000

    operators done

--- proj1/src/ai/node.py
+++ proj1/src/ai/node.py
@@ -1,3 +1,8 @@
+from operators import *
+
+WHITE_PLAYER = 'W'
+BLACK_PLAYER = 'B'
+
 class Node:
     """
     Class used to represent each board state/node.
@@ -9,7 +14,7 @@ class Node:
         self.board = board
         self.board_size = board_size
         self.parent = None
-
+        self.depth = 0
 
     def get_board(self):
         """
@@ -23,15 +28,19 @@ class Node:
         Function to set the parent of the state.
         """
         self.parent = parent
-
+        self.depth = self.parent.depth + 1
 
     def is_final_state(self, current_player):
         """
         Determines whether the state is final.
         """
+        neutron_x, neutron_y = get_neutron_coordinates(self)
+        if current_player == WHITE_PLAYER:
+            return does_white_player_win(neutron_y)
+        elif current_player == BLACK_PLAYER:
+            return  does_black_player_win(neutron_y)
 
-
-    def get_neutron_piece(self):
+    def get_neutron_coordinates(self):
         """
         Method that returns the coordinates of the neutron on the board.
         """
@@ -40,45 +49,28 @@ class Node:
                 if self.board[i][j] == NEUTRON_CHAR:
                     return j, i
 
-
-    def does_player_a_win(self, neutron_square_y):
+    def does_white_player_win(self, neutron_x, neutron_y):
         """
         Method that checks if player A has won.
         """
         if neutron_square_y == self.board_size - 1:
             return True
-        return False
+        return not check_neutron_possible_moves(self, neutron_x, neutron_y)
 
-
-    def does_player_b_win(self, neutron_square_y):
+    def does_black_player_win(self, neutron_x, neutron_y):
         """
         Method that checks if player B has won.
         """
         if neutron_square_y == 0:
             return True
+        return not check_neutron_possible_moves(self, neutron_x, neutron_y)
+
+    def check_neutron_possible_moves(self, neutron_x, neutron_y):
+
+        for op in operators:
+            if op(self, neutron_x, neutron_y) != None:
+                return True
         return False
-
-
-    def check_game_end(self):
-        """
-        Method that checks if the game has ended, and if so what was the result.
-        """
-        neutron_square_x, neutron_square_y = self.get_neutron_piece()
-        if self.does_player_a_win(neutron_square_y):
-            return True , PlayState.PLAYER_A_WINS
-
-        elif self.does_player_b_win(neutron_square_y):
-            return True, PlayState.PLAYER_B_WINS
-
-        else:
-            possible_moves_neutron = self.possible_moves(neutron_square_x, neutron_square_y)
-            if len(possible_moves_neutron) == 0:
-                if self.play_state == PlayState.PLAYER_A_MOVING_SOLDIER:
-                    return True, PlayState.PLAYER_A_WINS
-                elif self.play_state == PlayState.PLAYER_B_MOVING_SOLDIER:
-                    return True, PlayState.PLAYER_B_WINS
-
-        return False, None
 
 
 
